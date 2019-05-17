0313e7be1b0a20254887bb598f69ace1b7d6eb9b
EthanNew <shiminniu@163.com>
2019-05-17 23:55:22 +0800

    17晚提交

--- src/main/java/com/erp4j/ssm/service/impl/OrderServiceImpl.java
+++ src/main/java/com/erp4j/ssm/service/impl/OrderServiceImpl.java
@@ -1,9 +1,14 @@
 package com.erp4j.ssm.service.impl;
 
 import com.erp4j.ssm.mapper.COrderMapper;
+import com.erp4j.ssm.pojo.COrder;
+import com.erp4j.ssm.pojo.COrderExample;
 import com.erp4j.ssm.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @Author: Ethan New
@@ -18,7 +23,16 @@ public class OrderServiceImpl implements OrderService {
     COrderMapper orderMapper;
 
     @Override
-    public void find() {
+    public List<COrder> findRows() {
+        COrderExample example = new COrderExample();
+        List<COrder> cOrders = orderMapper.selectByExample(example);
+        return cOrders;
+    }
 
+    @Override
+    public int findTotal() {
+        COrderExample example = new COrderExample();
+        int count = (int) orderMapper.countByExample(example);
+        return count;
     }
 }
