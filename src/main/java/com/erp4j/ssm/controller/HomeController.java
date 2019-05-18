a2eef77c046351b12554d3485e44807a9b1fb181
yangpan <im_yangpan@163.com>
2019-05-18 15:36:59 +0800

    添加权限控制

--- src/main/java/com/erp4j/ssm/controller/HomeController.java
+++ src/main/java/com/erp4j/ssm/controller/HomeController.java
@@ -3,6 +3,10 @@ package com.erp4j.ssm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+
 /**
  * @Author: Ethan New
  * @Date: 2019/5/17 18:02
@@ -13,7 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/home")
-    public String home() {
+    public String home(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        ArrayList<String> list = new ArrayList<>();
+        /*添加权限*/
+        list.add("unqualify:add");
+        list.add("unqualify:edit");
+        list.add("unqualify:delete");
+
+        session.setAttribute("sysPermissionList",list);
         return "home";
     }
 }
